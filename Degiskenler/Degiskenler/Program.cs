e5340cddd6dfb034f0c8f8b60c77540539f86dea
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-08-15 13:58:00 +0300

    Degisler

--- Degiskenler/Degiskenler/Program.cs
+++ Degiskenler/Degiskenler/Program.cs
@@ -10,7 +10,34 @@ namespace Degiskenler
     {
         static void Main(string[] args)
         {
-            var a;
+            //Application
+            var studentName = "Ali ";
+            var studentSurname = "Turan";
+            var studentNameSurname= studentName+studentSurname;
+            var studentNumber = 123;
+            var studentGender = "male";
+            var studentTc = "1111111";
+            var studentBirthyear = 2005;
+            var studentAge=2024-studentBirthyear;
+            var studentAddress = "Kocaeli/izmit";
+
+            Console.WriteLine(studentName);
+            Console.WriteLine(studentSurname);
+            Console.WriteLine(studentNameSurname);
+            Console.WriteLine(studentNumber);
+            Console.WriteLine(studentGender);
+            Console.WriteLine(studentTc);
+            Console.WriteLine(studentBirthyear);
+            Console.WriteLine(studentAge);
+            Console.WriteLine(studentAddress);
+            Console.ReadKey();
+
+
+
+
+
+
+
 
         }
     }
