86fc3ef00a0c0147d3acacc10a99332411f9a076
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-08-15 13:45:11 +0300

    For hello world

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
         }
     }
 }
