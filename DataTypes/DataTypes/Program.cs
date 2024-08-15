79e3fcda8166132edc69cb4c4cc5aec7771c1d09
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-08-15 18:29:36 +0300

    For data types project

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            C# Veri Tipleri
+            Value Types: 
+                tam sayı: byte,short,int,long
+                ondalıklı sayılar: float,double,decimal
+                diğer veri tipleri: char,boolean,struct
+            Reference Types:
+                String,Class,Array,Interface      
+             */
+
+
+
+
+        }
+    }
+}
