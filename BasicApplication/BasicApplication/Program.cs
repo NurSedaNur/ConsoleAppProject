136f2857881e4c27f38f8fcba3dd9325bffdd8e9
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-08-24 23:11:20 +0300

    basic application done

---
+++ BasicApplication/BasicApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayi giriniz:");
+            var sayi = Convert.ToInt32(Console.ReadLine());
+
+            string[] dizi = { "a", "b", "c", "d" };
+            for (int i = 0; i < sayi; i++)
+            {
+                var sayıMod = i % 4;
+                Console.WriteLine(dizi[sayıMod]);
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
