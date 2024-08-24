e78669ba0c33f312c3d25ea647ed6a7cf467ff51
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-08-24 23:46:57 +0300

    OOP first application finish.

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -10,9 +10,37 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            //class=>object(ogr1,ogr2)
+            Ogrenci ogr1 = new Ogrenci() { OgrenciNo = "100", AdSoyad = "Ada Bilgi", Sube = "6/A" };
+
+            Ogrenci ogr2 = new Ogrenci() { OgrenciNo = "200", AdSoyad = "Yigit Bilgi" , Sube = "7/A" };
+
+            Ogrenci ogr3 = new Ogrenci() { OgrenciNo = "300", AdSoyad = "Çınar Turan", Sube = "1/A" };
             
+            Ogrenci[] ogrenciler=new Ogrenci[3] {ogr1, ogr2, ogr3};
+            
+            foreach (var ogrenci in ogrenciler)
+            {
+                Console.WriteLine(ogrenci.bilgileriYazdir());
+            }
+            Console.ReadKey();
+        }
+    }
+    class Ogrenci
+    {
+        //property=>string,int
+        public string OgrenciNo { get; set; }
+        public string AdSoyad { get; set; }
+        public string Sube { get; set; }
 
-
+        //method=>bilgileriYazdir()
+        public string bilgileriYazdir()
+        {
+            string sonuc=($"{this.OgrenciNo} numaralı ogrencinin adı: {this.AdSoyad} ve subesi: {this.Sube}");
+            return sonuc;
         }
     }
 }
+//OOP=>Program,Main
+//System=>Console
+//System.IO=>File,Directory,Path
