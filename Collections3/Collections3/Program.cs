9ef517c9f4f6012be3a11fdbc26ea6a9418c0077
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-08-26 20:28:35 +0300

    Dictionary finish

---
+++ Collections3/Collections3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dictionary
+            // Key-Value => plaka-şehir
+
+            Dictionary<int,string> plakalar=new Dictionary<int,string>();
+            plakalar.Add(41, "kocaeli");
+            plakalar.Add(34, "istanbul");
+            plakalar.Add(59, "tekirdağ");
+
+            Dictionary<int,string> sayilar=new Dictionary<int, string>()
+            {
+                {1,"bir" },
+                {2,"iki" },
+                {3,"üç" }
+            };
+
+            Console.WriteLine(plakalar[41]);
+            if (plakalar.ContainsKey(61))
+            {
+                Console.WriteLine(plakalar[61]);
+            }
+
+            foreach (var plaka in plakalar)
+            {
+                Console.WriteLine(plaka.Key+" "+plaka.Value);
+            }
+
+            //update
+            sayilar[1] = "one";
+            sayilar.Remove(2);
+            //sayilar.Clear(); //bütün elemanları siler
+            foreach (var sayi in sayilar)
+            {
+                Console.WriteLine(sayi.Key + " " + sayi.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
