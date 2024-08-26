67e42d633bb581c2dd5aa42387a3651c6e39bab6
Seda Nur Ekici <sedanur.ekc.312@gmail.com>
2024-08-26 03:04:34 +0300

    static methods done

---
+++ StaticMethods/StaticMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sonuc = HelperMethods.KarakterDuzelt("ölçme ve değerlendirme");
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+        }
+    }
+
+    class HelperMethods
+    {
+        public static string KarakterDuzelt(string str)
+        {
+            return
+                str.Replace("ö", "o")
+                .Replace("ü", "u")
+                .Replace("ı", "i")
+                .Replace("ğ", "g")
+                .Replace("ç", "c")
+                .Replace(" ", "-");
+        }
+
+    }
+
+}
